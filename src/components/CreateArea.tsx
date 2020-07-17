d558519e329553d57f0d846ff56f6738558b4ee5
haweecodes <haweesajib10@gmail.com>
2020-07-17 15:50:54 +0600

    Initial body layout designed

---
+++ src/components/CreateArea.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CreateArea.css';
+
+export default class CreateArea extends Component {
+    
+    render(){
+        return(
+            <section>
+                {/* edit area */}
+                <h1>Create area</h1>
+            </section>
+        )
+    };
+}
---
+++ src/components/ToolBar.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './ToolBar.css';
+
+export default class ToolBar extends Component {
+    render(){
+        return(
+            <aside>
+                {/* title */}
+                <h1>ToolBox</h1>
+                {/* general tools */}
+
+                {/* other tools */}
+            </aside>
+        )
+    };
+}
