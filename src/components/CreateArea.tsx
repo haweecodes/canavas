424bc9a8f86ce5c6a3d67ca14bf8c16a194a9105
haweecodes <haweesajib10@gmail.com>
2020-07-21 00:20:20 +0600

    elements added to the sidebar

--- src/components/CreateArea.tsx
+++ src/components/CreateArea.tsx
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './CreateArea.css';
 
-export default class CreateArea extends Component {
-    
-    render(){
-        return(
-            <section>
-                {/* edit area */}
-
-                <div className="main_wrapper">
-
-                </div>
-            </section>
-        )
-    };
+type Props = {
+    element: string;
 }
+const CreateArea: React.FC<Props> = ({element}) => {
+    
+    // check if element is empty or not
+    // if(!!element) {
+    //         let newElement = document.createElement(element);
+
+    //         switch(element){
+    //             case 'button':
+    //                 newElement.innerHTML = 'Click Me';
+    //                 break;
+    //             case 'input':
+    //                 // type casting from html element to input element
+    //                 (newElement as HTMLInputElement).value = 'Type something';
+    //                 break;
+    //             default:
+    //                 newElement.innerHTML = 'New Text';
+    //             }
+                
+    //         // get the box element from the section area
+    //         let boxElement = document.getElementById('boxElement');
+    //         boxElement?.appendChild(newElement);
+    // }
+    return(
+        <section>
+            {/* edit area */}
+            <div className="main_wrapper" id="boxElement"></div>
+        </section>
+    )
+}
+
+
+export default CreateArea;
--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ToolBar.css';
 
-export default class ToolBar extends Component {
-    render(){
-        return(
-            <aside>
-                {/* title */}
-                <h1>ToolBox</h1>
-                {/* general tools */}
-                <div className="tools_wrapper">
-                    <Tools />
-                    <Tools />
-                </div>
-                {/* other tools */}
-            </aside>
-        )
-    };
-}
-
-function Tools() {
-    return(
-        <div className="tool">
-            <span>Textbox</span>
-        </div>
+export default function ToolBar(props: any) {
+    // left toolbar view with all the tools
+    return (
+        <aside>
+            {/* title */}
+            <h1>ToolBox</h1>
+            {/* general tools */}
+            <div className="tools_wrapper">
+                <Tools name='rectangle' callBack={props.emitCreateElement} />
+                <Tools name='circle' callBack={props.emitCreateElement} />
+                <Tools name='square' callBack={props.emitCreateElement} />
+            </div>
+            {/* other tools */}
+        </aside>
     )
 }
+
+type Props = {
+    name: string;
+    callBack: any;
+}
+const Tools: React.FC<Props> = ({
+    name,
+    callBack
+}) => (
+        <button className="tool" onClick={() => callBack(name)}>
+            <span>{name}</span>
+        </button>
+    )
